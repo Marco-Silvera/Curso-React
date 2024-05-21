e8bc945b963f8e97049577048c4f814dd50b7d3c
Marco Silvera <2018019183@unfv.edu.pe>
2024-05-20 20:06:13 -0500

    Creación de viaje a tu medida

---
+++ src/components/ViajeAMedida.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './ViajeAMedida.css'
+
+function ViajeAMedida() {
+    return (
+        <section className='ViajeAMedida-container'>
+            <img className='ViajeAMedida-img' src="/src/assets/viaje-a-tu-medida.webp" alt="Machu Picchu Maravilla del mundo" />
+            <div className='ViajeAMedida-textos'>
+                <h3 className='ViajeAMedida-title'>
+                    Viaja a tu medida
+                </h3>
+                <p className='ViajeAMedida-description'>
+                    Personaliza tu viaje y descubre destinos turísticos con el sello global de SAFE TRAVELS
+                </p>
+                <a className='ViajeAMedida-button' href="">Más Información</a>
+            </div>
+        </section>
+
+
+    )
+}
+
+export default ViajeAMedida
